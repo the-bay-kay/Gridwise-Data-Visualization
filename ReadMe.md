a9056227539da8f73f1c6c9633afa88d415a4a0a
Katie Rischpater <98350084+the-bay-kay@users.noreply.github.com>
2024-03-06 19:04:47 -0800

    Initialized the repository - Added existing `setup/` script - Added first notebook `parse_gridwise_data.ipynb`

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Parsing Gridwise Data
+
+## Introduction
+This repository holds Jupyter notebook(s) that I've used to  parse, analyze, and visualize the data provided to [NREL](https://www.nrel.gov/index.html) by [Gridwise](https://gridwise.io/).  For more information on this project, please contact Katie.Rischpater@nrel.gov
+
+## Running the notebooks
+To ensure all of the correct packages are installed, feel free to use the conda environment provided in the `setup/` folder! Afterwards, run the jupyter notebooks through your preferred means (VSCode, jupyter server, etc.)
---
+++ setup/setup-conda.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# To run: 
+#   $: cd /path/to/Gridwise_Data_Visualization/
+#   $: bash setup/setup-conda.sh
+
+# To activate environment: 
+#   $: conda activate hive_extension
+
+# Author's Note: The location & Version of Conda were chosen
+# to be the same as those used in the OpenPATH `e-mission-server`.
+
+export EXP_CONDA_VER=23.5.2
+export EXP_CONDA_VER_SUFFIX=0
+
+INSTALL_PREFIX=$HOME/miniconda-$EXP_CONDA_VER
+SOURCE_SCRIPT="$HOME/miniconda-$EXP_CONDA_VER/etc/profile.d/conda.sh"
+source $SOURCE_SCRIPT
+
+conda env update --name hive_extension --file setup/hive_conda.yml
